7c68ae7d9b5f14226a5f2c53f2c81739a4ad5694
IJUYJIU <95621220+IJUYJIU@users.noreply.github.com>
2021-12-06 20:57:18 +0800

    Update tank.py

--- tank.py
+++ tank.py
@@ -7,20 +7,20 @@ temp = st.number_input('溫度的資料收集:限溫30 =>')
 轉速= st.number_input('轉速資料的收集:限轉12000 =>')
 confirm_input = st.button("ok")
 if confirm_input:
-if gas <= car_simulation.get('gas_warning'):
-  print('油箱只剩', gas, '格! 準備加油!!')
-else:
-  print('油箱還剩', gas, '格。')
-if speed>=car_simulation.get('speed_limit'):
-  print('即將超速')
-else:
-    print('安全')
-if temp>=car_simulation.get('temp_warning'):
-    print('過熱')
-else:
-    print('正常')
-if 轉速>=car_simulation.get('轉速'):
+  if gas <= car_simulation.get('gas_warning'):
+    print('油箱只剩', gas, '格! 準備加油!!')
+  else:
+    print('油箱還剩', gas, '格。')
+  if speed>=car_simulation.get('speed_limit'):
+    print('即將超速')
+  else:
+      print('安全')
+  if temp>=car_simulation.get('temp_warning'):
+      print('過熱')
+  else:
+      print('正常')
+  if 轉速>=car_simulation.get('轉速'):
       print('即將超過轉速')
-else:
-    print('正常')
+  else:
+      print('正常')
   
